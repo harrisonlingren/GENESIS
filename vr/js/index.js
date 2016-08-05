6b74324155d87969947591b3ae03c6ac6aa7f026
Lingren, Harrison <harrison.lingren@nextgearcapital.com>
2016-08-05 15:03:54 -0400

    remove options from component

--- vr/js/index.js
+++ vr/js/index.js
@@ -2,22 +2,9 @@
 // THREE.terrain by IceCreamYou (https://github.com/IceCreamYou/THREE.Terrain)
 
 AFRAME.registerComponent('terrain', {
-  schema: {frequency: {default: 2.5}},
+  schema: {},
   init: function () {
-    this.terrainScene = THREE.Terrain({
-      easing: THREE.Terrain.Linear,
-      frequency: this.data.frequency,
-      heightmap: THREE.Terrain.DiamondSquare,
-      material: new THREE.MeshBasicMaterial({color: 0x5566aa}),
-      maxHeight: 100,
-      minHeight: -100,
-      steps: 1,
-      useBufferGeometry: false,
-      xSegments: xS,
-      xSize: 1024,
-      ySegments: yS,
-      ySize: 1024,
-    });
+    this.terrainScene = THREE.Terrain();
     this.el.setObject3D('mesh', this.terrainScene);
   }
 });
