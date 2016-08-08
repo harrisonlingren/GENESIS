a11dbf80812fe046e16c3c3f1355fe7c72eac18d
Conlin Durbin <conlindurbin@gmail.com>
2016-08-08 16:17:58 -0400

    merging and fixing conflicts

--- vr/js/index.js
+++ vr/js/index.js
@@ -1,26 +1,30 @@
 // Test Procedural Terrain
 // THREE.terrain by IceCreamYou (https://github.com/IceCreamYou/THREE.Terrain)
-
-AFRAME.registerComponent('terrainScene', {
-  schema: {frequency: {default: 2.5}},
-  init: function () {
-    this.terrainScene = THREE.Terrain({
-      easing: THREE.Terrain.Linear,
-      frequency: this.data.frequency,
-      heightmap: THREE.Terrain.PerlinDiamond,
-      material: new THREE.MeshBasicMaterial({color: 0x5566aa}),
-      maxHeight: 88,
-      minHeight: -100,
-      width: 1,
-      length: 1,
-      size: 2048,
-      steps: 7,
-      useBufferGeometry: false,
-      xSegments: xS,
-      xSize: 1024,
-      ySegments: yS,
-      ySize: 1024,
+function start() {
+  if(THREE.terrain) {
+    AFRAME.registerComponent('terrain', {
+      schema: {frequency: {default: 2.5}},
+      init: function () {
+        this.terrainScene = THREE.Terrain({
+          easing: THREE.Terrain.Linear,
+          frequency: this.data.frequency,
+          heightmap: THREE.Terrain.DiamondSquare,
+          material: new THREE.MeshBasicMaterial({color: 0x5566aa}),
+          maxHeight: 100,
+          minHeight: -100,
+          steps: 1,
+          useBufferGeometry: false,
+          xSegments: xS,
+          xSize: 1024,
+          ySegments: yS,
+          ySize: 1024,
+        });
+        this.el.setObject3D('mesh', this.terrainScene);
+      }
     });
-    this.el.setObject3D('mesh', this.terrainScene);
+  } else {
+    setTimeout(start, 500);
   }
-});
+}
+
+start();
